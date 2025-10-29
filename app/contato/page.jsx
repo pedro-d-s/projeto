c32affedf905adb06db007ce0871054239376fdf
Pedro Domingues Santos <pedrosanttzz2012@gmail.com>
2025-10-28 22:20:05 -0300

    Inclusão da página crua de contato
    Não há nada além de uma frase nessa aba. Pendente de mais interações...

---
+++ app/contato/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useState } from "react"
+
+export default function Contato() {
+    return (
+        <div className="w-full flex flex-col items-center">
+            <div className="w-8/10 h-dvh flex flex-col justify-center">
+                <h1 className="text-3xl w-full text-center">Deixe suas informações. Logo entraremos em contato.</h1>
+            </div>
+        </div>
+    )
+}
